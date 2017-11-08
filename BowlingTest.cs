a0590f9f07bf87dbc0dc5abcac2c25c344eabb4b
Kiki <kiki2002kimo@gmail.com>
2017-11-09 01:18:23 +0800

    Add Test:: WhenBowling10RoundGetSecondStrike_ReturnScore (FAILED)

--- BowlingTest.cs
+++ BowlingTest.cs
@@ -129,5 +129,29 @@ namespace Bowling
 
             Assert.AreEqual(expected, Bowling.Score());
         }
+
+        [Test]
+        public void WhenBowling10RoundGetSecondStrike_ReturnScore()
+        {
+            var Bowling = new Bowling();
+
+            Bowling.Roll(1, 0); //1
+            Bowling.Roll(10, 0); //0 -> 11 + 1 = 12
+            Bowling.Roll(1, 0); //13
+            Bowling.Roll(1, 0); //14
+            Bowling.Roll(10, 0); //25
+            Bowling.Roll(1, 0); //26
+            Bowling.Roll(1, 0); //27
+            Bowling.Roll(1, 0); //28
+            Bowling.Roll(1, 0); //29
+
+            Bowling.Roll(10, 0); //46
+            Bowling.Roll(10, 0); //46
+            Bowling.Roll(3, 0); //Sencond = -
+
+            var expected = 52;
+
+            Assert.AreEqual(expected, Bowling.Score());
+        }
     }
 }
