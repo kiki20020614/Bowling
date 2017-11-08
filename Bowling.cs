8cd273c82b1107201ece52a43e74e91273da9b30
Kiki <kiki2002kimo@gmail.com>
2017-11-09 00:07:35 +0800

    Add spareScore Logic, Test :: WhenBowlingGetSpare_ReturnScore (PASS)

--- Bowling.cs
+++ Bowling.cs
@@ -3,11 +3,23 @@
     public class Bowling
     {
         public readonly int Rounds = 10;
+
+        private readonly int maxScoreOfRound = 10;
+        private int spareScore;
         private int score;
 
         public int Roll(int first, int second)
         {
-            score = score + first + second;
+            if (first + second == maxScoreOfRound)
+            {
+                spareScore = maxScoreOfRound;
+            }
+            else
+            {
+                spareScore = spareScore == maxScoreOfRound ? maxScoreOfRound + first : 0;
+                score = score + first + second + spareScore;
+            }
+
             return score;
         }
 
